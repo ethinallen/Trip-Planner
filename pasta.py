15188523481dc2960f729f925249ddda4ea18f38
ethinallen <abemery@clemson.edu>
2019-02-13 11:28:11 -0500

    Add files via upload
    upload the code I made at work

--- pasta.py
+++ pasta.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
+import requests, config
 
 ###########################
 # Problem Data Definition #
@@ -8,46 +9,55 @@ from ortools.constraint_solver import routing_enums_pb2
 def create_data_model():
   """Stores the data for the problem"""
   data = {}
-  # Array of distances between locations.
-  _distances = \
-          [
-           [0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354, 468, 776, 662],
-           [548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674, 1016, 868, 1210],
-           [776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164, 1130, 788, 1552, 754],
-           [696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822, 1164, 560, 1358],
-           [582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708, 1050, 674, 1244],
-           [274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628, 514, 1050, 708],
-           [502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856, 514, 1278, 480],
-           [194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320, 662, 742, 856],
-           [308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662, 320, 1084, 514],
-           [194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388, 274, 810, 468],
-           [536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764, 730, 388, 1152, 354],
-           [502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114, 308, 650, 274, 844],
-           [388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194, 536, 388, 730],
-           [354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0, 342, 422, 536],
-           [468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536, 342, 0, 764, 194],
-           [776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274, 388, 422, 764, 0, 798],
-           [662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730, 536, 194, 798, 0]
-          ]
 
-  demands = [0, 1, 1, 3, 6, 3, 6, 8, 8, 1, 2, 1, 2, 6, 6, 8, 8]
-  capacities = [15, 15, 15, 15]
+  # create the locations and demands
+  locations = open('locations.txt', 'r').read().splitlines()
+  demands = open('weights.txt', 'r').read().splitlines()
+  length = len(demands)
+  for i in range(0, length):
+      demands[i] = int(demands[i])
+
+  # creates the parts of the url for the get request
+  key = '&key=' + config.apiKey
+
+  baseURL = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+
+  origin = 'origins='
+
+  destinations = '&destinations='
+  
+  for location in locations:
+      destinations += location.replace(' ', '') + '|'
+
+  _distances = []
+
+  tempList = []
+
+  for lcoation in locations:
+      request = requests.get(baseURL + origin + location.replace(' ', '') + destinations + key).json()
+      request = request['rows'][0]['elements']
+      for elem in request:
+          print(elem)
+          tempList.append(int(elem['duration']['value']))
+      _distances.append(tempList)
+      tempList = []
+  
+  capacities = [700]
   data["distances"] = _distances
   data["num_locations"] = len(_distances)
-  data["num_vehicles"] = 4
+  data["num_vehicles"] = 1
   data["depot"] = 0
   data["demands"] = demands
   data["vehicle_capacities"] = capacities
   return data
+
 #######################
 # Problem Constraints #
 #######################
-def manhattan_distance(position_1, position_2):
-  """Computes the Manhattan distance between two points"""
-  return (
-      abs(position_1[0] - position_2[0]) + abs(position_1[1] - position_2[1]))
-def create_distance_callback(data):
-  """Creates callback to return distance between points."""
+#def manhattan_distance(position_1, position_2):
+#  """Computes the Manhattan distance between two points"""
+#  return (abs(position_1[0] - position_2[0]) + abs(position_1[1] - position_2[1]))
+
 def create_distance_callback(data):
   """Creates callback to return distance between points."""
   distances = data["distances"]
@@ -72,6 +82,14 @@ def add_capacity_constraints(routing, data, demand_callback):
         data["vehicle_capacities"], # vehicle maximum capacities
         True, # start cumul to zero
         capacity)
+    
+def add_distance_dimension(routing, distance_callback):
+    distance = 'Distance'
+    maximum_distance = 648000
+    routing.AddDimension(distance_callback, 0, maximum_distance, True, distance)
+    distance_dimension = routing.GetDimensionOrDie(distance)
+    #distance_dimension.SetGlobalSpanCostCoefficient(100)
+
 ###########
 # Printer #
 ###########
@@ -79,7 +97,7 @@ def print_solution(data, routing, assignment):
     """Print routes on console."""
     total_dist = 0
     visited_locations = []
-    dropped_locations = range(data["num_locations"])
+    dropped_locations = list(range(data["num_locations"]))
 
     for vehicle_id in range(data["num_vehicles"]):
         index = routing.Start(vehicle_id)
@@ -105,6 +123,7 @@ def print_solution(data, routing, assignment):
         print(plan_output)
     print('Total Distance of all routes: {0}m'.format(total_dist))
     print('Dropped visits: ', list(set(range(data["num_locations"])) - set(visited_locations)))
+
 ########
 # Main #
 ########
@@ -119,6 +138,7 @@ def main():
       data["depot"])
   # Define weight of each edge
   distance_callback = create_distance_callback(data)
+  add_distance_dimension(routing, distance_callback)
   routing.SetArcCostEvaluatorOfAllVehicles(distance_callback)
   # Add Capacity constraint
   demand_callback = create_demand_callback(data)
@@ -129,7 +149,7 @@ def main():
       routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
 
   # Adding penalty costs to allow dropping visits.
-  penalty = 1000
+  penalty = 1000000
   for i in range(1, data["num_locations"]):
     routing.AddDisjunction([routing.NodeToIndex(i)], penalty)
   # Solve the problem.
@@ -138,4 +158,4 @@ def main():
     print_solution(data, routing, assignment)
 
 if __name__ == '__main__':
-  main()
+    main()
